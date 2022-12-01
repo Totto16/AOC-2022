160ded693dead90965d93767f08055dbf22aef37
Totto16 <tobiausgais@gmail.com>
2022-12-01 08:51:03 +0100

    solve Day 01

--- src/Day 01/index.ts
+++ src/Day 01/index.ts
@@ -1,18 +1,38 @@
 import { start, getFile } from '../utils';
 
+function parseElves(input: string[]): number[][] {
+    const elves: number[][] = [[]];
+    let i = 0;
+    for (const inp of input) {
+        if (inp === '') {
+            ++i;
+            elves[i] = [];
+            continue;
+        }
+
+        elves[i].push(parseInt(inp.trim()));
+    }
+
+    return elves;
+}
+
 function solve(input: string[]): number {
-    return -1;
+    const elves = parseElves(input).map((a) => a.reduce((acc, b) => acc + b, 0));
+    elves.sort((a, b) => b - a);
+    return elves[0];
 }
 
 function solve2(input: string[]) {
-    return -1;
+    const elves = parseElves(input).map((a) => a.reduce((acc, b) => acc + b, 0));
+    elves.sort((a, b) => b - a);
+    return elves.slice(0, 3).reduce((acc, b) => acc + b, 0);
 }
 
 function TestBoth() {
-    const testInput = getFile('./sample.txt', __filename);
+    const testInput = getFile('./sample.txt', __filename, '\n', false);
 
-    const testResult = 12;
-    const testResult2 = 4;
+    const testResult = 24000;
+    const testResult2 = 45000;
 
     const test = solve(testInput);
     if (test !== testResult) {
@@ -27,4 +47,10 @@ function TestBoth() {
     }
 }
 
-start(__filename, { tests: TestBoth, solve, solve2 }, { needsPrototypes: true });
+start(
+    __filename,
+    { tests: TestBoth, solve, solve2 },
+    { needsPrototypes: true, inputOptions: { filterOutEmptyLines: false, separator: '\n' } }
+);
+
+//npm run compile && node . --1 -d
